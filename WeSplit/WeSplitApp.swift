126e6d5be008eb5991ef3af66f5b121c43b87cb5
ValentinFLGT <valentin.flgt@gmail.com>
2023-08-10 08:07:57 +0200

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Valentin FLGT on 09/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
